3eb0b998e8cbe50c85a6592d4b5285db8aa552c5
HM-Tanjil9 <rtanjil0@gmail.com>
2025-05-05 07:54:33 +0600

    making the paddle move

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     let table = document.getElementById('ping-pong-table');
+    let paddle = document.getElementById('paddle');
     let ball = document.getElementById('ball');
     // ball position
     let ballX = 10;
@@ -19,7 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
         ball.style.left = `${ballX}px`;
         ball.style.top = `${ballY}px`;
 
-        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx = -dx;
-        if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy = -dy;
+        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
+        if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
     }, 5);
+
+
+    let paddleY = 0;
+    let dPy = 5;
+    document.addEventListener('keydown', (event) => {
+        if(event.keyCode == 38 && paddleY > 0) {
+            // up arrow pressed
+            paddleY += (-1)*dPy;
+            console.log('up');
+            
+
+        } else if(event.keyCode == 40 && paddleY < table.offsetHeight - (paddle.offsetHeight + dPy)) {
+            // down arrow pressed
+            paddleY += dPy;
+        }
+        paddle.style.top = `${paddleY}px`;
+    })
 });
